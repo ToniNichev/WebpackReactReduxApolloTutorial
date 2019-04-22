21fd63cab71370cbde663246db6df76638cac5ae
ToniNichev <toni.nichev@gmail.com>
2019-04-22 19:50:38 -0400

    Work

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import Home from '../Home';
 import Greetings from '../Greetings';
 import About from '../About';
+import Header from '../Header';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import styles from './styles.scss';
@@ -9,7 +11,9 @@ export default class App extends Component {
     return (
       <div className={styles.appWrapper}>
         <Router>
+          <Header />
           <Switch>
+            <Route exact path="/home" component={Home} />            
             <Route exact path="/greetings" render={(props) => <Greetings {...props} user="John" />} />
             <Route exact path="/about" component={About} />
           </Switch>
