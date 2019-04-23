dfbd7a6585316ac537c61da8ef81c57899765e58
Toni Nichev <toni.nichev@nbcuni.com>
2019-04-23 10:27:44 -0400

    Work

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
-import Home from '../Home';
 import Greetings from '../Greetings';
-import About from '../About';
-import Header from '../Header';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import About from '../About';
+import { ApolloProvider, graphql } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import DogsCatalog from '../../containers/DogsCatalog';
+import Header from '../Header';
 import styles from './styles.scss';
 export default class App extends Component {
   render() {
+    const GRAPHQL_URL = 'http://localhost:4001/graphql';
+    const client = new ApolloClient({
+      link: new HttpLink({ uri:  GRAPHQL_URL }),
+      cache: new InMemoryCache()
+    });  
     return (
       <div className={styles.appWrapper}>
-        <Router>
-          <Header />
-          <Switch>
-            <Route exact path="/home" component={Home} />            
-            <Route exact path="/greetings" render={(props) => <Greetings {...props} user="John" />} />
-            <Route exact path="/about" component={About} />
-          </Switch>
-        </Router>
+        <ApolloProvider client={client}>
+          <Router>
+            <Header />
+            <Switch>
+              <Route exact path="/greetings" user="Test" component={Greetings} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/dogs" component={DogsCatalog} />
+            </Switch>
+          </Router>
+        </ApolloProvider>
       </div>        
     );
   }
