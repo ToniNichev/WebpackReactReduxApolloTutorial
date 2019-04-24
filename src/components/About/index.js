9389a64e96decffe5aa8b779f658e418d9e8014d
Toni Nichev <toni.nichev@nbcuni.com>
2019-04-24 15:11:34 -0400

    Work

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,40 @@
-import React from 'react';
-const About = () => (
-  <div>
-    <div>Wellcome to my tutorial!</div>
-  </div>
-)
-export default About;
+import React, { Component } from 'react';
+import connect from '../../containers/Provider/connect.js';
+
+const CHANGE_USERNAME = 'CHANGE_USERNAME';
+class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: this.props.userName,
+    };    
+  }
+  handleChange() {
+    const userName = document.querySelector('input[name=username]').value;
+    this.setState( { userName: userName } );
+    this.props.onEdit(userName);
+  }
+  render() {
+    return (
+      <div>
+        <p>This is <input type="text" name="username" value={this.state.userName} onChange={() => { this.handleChange()}} /></p>
+      </div>
+    );
+  }
+}
+//export default About;
+const mapStateToProps = storeState => ({
+  userName: storeState.userName
+}
+);
+const mapDispatchToProps = dispatch => ({
+  onEdit: (userName) => dispatch({
+    type: CHANGE_USERNAME,
+    data: userName
+  })
+});
+const AboutContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(About);
+export default AboutContainer;
