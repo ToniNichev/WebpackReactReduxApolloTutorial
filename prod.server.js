b319af5ba6be14ffac9c15fbedab894cac3eadf6
Toni Nichev <toni.nichev@nbcuni.com>
2019-05-17 16:08:25 -0400

    #Stable with build all

--- prod.server.js
+++ prod.server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-const port = 8080
+const port = 8080;
 app.use('/dist', express.static('dist'));
 app.get('*', (req, res)=>{
   res.sendFile(path.join(__dirname, 'index.html'));
