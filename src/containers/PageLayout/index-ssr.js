0d8f09725262b6d5933f67e821d9066c8181858d
Toni Nichev <toni.nichev@nbcuni.com>
2019-06-12 13:59:51 -0400

    #STABLE refactoring pageLayout SSR component

--- src/containers/PageLayout/index-ssr.js
+++ src/containers/PageLayout/index-ssr.js
@@ -1,79 +1,38 @@
 import React, { Component } from 'react';
-//import ComponentList from './ComponentList';
 import PagesLayoutData from './pages-layout-data';
-//import Loading from '../../components/Loading';
-
-//const header = import('../../components/Header');
 
 const styles = require('./styles.scss');
-const isSSR = typeof window === 'undefined' ? true : false;
 
-const ComponentList = {
-  Header : 'Header',
-  Home: 'Home',
-  Greetings: 'Greetings'
-}
+export default (props) => {
 
-class PageLayout extends Component {
-
-    constructor(props) {
-      super(props);      
-
-      const url = props.location.pathname;
-      const componentsList = this.getComponentsList(url);
-      this.state = {
-        url: url,
-        componentsList: componentsList,
-        components: []
-      }
-    } 
-
-    componentWillReceiveProps(nextProps) {
-      const url = nextProps.location.pathname;
-      const componentsList = this.getComponentsList(url);
-      this.state.components = []; // force changing the state right away otherwise react will queue the request and we will end up with duplicate components
-      this.setState({ components: [], url: url, componentsList: componentsList });      
-      this.setComponents(componentsList);
-
-    }
+  function getComponentsList(url) {
+    return PagesLayoutData[url];
+  }
   
-    getComponentsList(url) {
-      return PagesLayoutData[url];
-    }
+  const url = props.location.pathname;
 
-    setComponents(componentsList) {
-  
-      let components = this.state.components;
-      componentsList.map((componentName, id , allLomponents) => {
-        const componentPath = ComponentList[componentName]; 
+  function getComponents(componentsList) {  
+    let components = componentsList.map((componentName, id , allLomponents) => {
+      // works on SSR but doesn't split the bundle since webpack is considering all files necessary to load.
+      // this creates `server-build/main-bundle.js` that will have the JS code for all components, which we are NOT going to use.
+      // We just need the components source code on the server side.
+      let component = require('../../components/' + componentName);
+      const componentWithWrapper = <component.default key={id} />;
+      return componentWithWrapper;
+      
+    });
+    return components;
+  }
 
-        // works on SSR but doesn't split the bundle since webpack is considering all files necessary to load.
-        // this creates `server-build/main-bundle.js` that will have the JS code for all components, which we are NOT going to use.
-        // We just need the components source code on the server side.
-        let component = require('../../components/' + componentPath);
-        components.push(<component.default key={id} />);
-        this.setState({components: components});
-        
-      });
-    }
+  const componentsList = getComponentsList(url);
+  const components = getComponents(componentsList);
+  const allComponents = components.map( (component) => {
+    return component;
+  });
 
-    componentDidMount() {
-      this.setComponents(this.state.componentsList);
-    }
-
-    render() {
-
-      if(typeof window == 'undefined')
-        this.setComponents(this.state.componentsList);
-      const allComponents = this.state.components.map( (component, id, components) => {
-        return component;
-      });
-
-      return(
-        <div className={styles.app}>
-          {allComponents}
-        </div>
-      );
-    }
+  return(
+    <div className={styles.wrapper}>
+      {allComponents}
+    </div>
+  );
 }
-export default PageLayout;
