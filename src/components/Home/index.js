972b99027d4920b481f57f9324922dbdb562cf44
Toni Nichev <toni.nichev@nbcuni.com>
2019-06-12 11:53:17 -0400

    #stable

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import ShowText from '../ShowText'
 const styles = require('./styles.scss');
 const Home = () => (
   <div>
-    <div className={styles.wrapper}>This is my home section!</div>
+    <div className={styles.wrapper}>This is my <ShowText textToShow="HOME"/> section!</div>
   </div>
 )
 export default Home;
