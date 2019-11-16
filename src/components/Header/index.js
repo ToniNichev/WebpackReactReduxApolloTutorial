b3fff2509ba4b1bd0b18c644bde143205d76571c
ToniNichev <toni.nichev@gmail.com>
2019-11-16 13:45:15 -0500

    Work

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-const styles = require('./styles.scss');
 
-
-const Header = ( {title} ) => (
+const Header = () => (
   <div>
-    <div className={styles.wrapper}>      
-      <h2>{ title } { process.env.APP_NAME } </h2>
-      <ul>
-        <li><Link to='/home'>HOME</Link></li>
-        <li><Link to='/greetings'>GREETINGS</Link></li>       
-        <li><Link to='/dogs-catalog'>DOGS CATALOG</Link></li>
-        <li><Link to='/about'>ABOUT</Link></li>
-      </ul>
+    <div>      
+      <h2>TEST 123</h2>
     </div>
   </div>
 );
