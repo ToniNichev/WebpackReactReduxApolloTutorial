272ad00b919ed29e8837910cb1f70e1eb3f7c546
Toni Nichev <toni.nichev@nbcuni.com>
2019-11-19 15:42:46 -0500

    Renaming thest file for Greetings component

--- src/components/Greetings/renderer.test.js
+++ src/components/Greetings/renderer.test.js
@@ -8,23 +8,33 @@ import Component from './renderer';
 
 const store = createStore(reducers, {});
 describe('Testing Greetings component', () => {
-  it('renders as expected!!!', () => {
-    const wrapper = mount(
-      <Provider store={store}>
-        <Component styles={{}} />
-      </Provider>
-    );
+
+  const wrapper = mount(
+    <Provider store={store}>
+      <Component styles={{}} />
+    </Provider>
+  );
+
+  it('renders as expected', () => {
     // to snapshot test
     expect(toJson(wrapper)).toMatchSnapshot();
-    // make sure that textbox does not exist
+  });
+
+  it('textbox does not exist', () => {  
     expect(wrapper.find('input').exists()).toBe(false);
+  });
+
+  it('textbox exists after h2 click', () => {  
     // simulate click on the h2 tag trigering props change and visualizing the input text box
     wrapper.find('h2').simulate('click');
     expect(wrapper.find('input').exists()).toBe(true);
+  });    
+
+  it('textbox values change properly', () => {    
     // text box value tests
     expect(wrapper.find('input').props().value).toBe('No Name');
     wrapper.find('input').props().value = 'test';
     expect(wrapper.find('input').props().value).toBe('test');
+  });    
 
- });
 });
