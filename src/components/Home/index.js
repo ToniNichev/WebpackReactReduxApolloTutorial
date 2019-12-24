f21ab08d2b2491c2ddcbcb97b2a5f48bf121d578
Toni Nichev <toni.nichev@nbcuni.com>
2019-12-24 14:01:08 -0500

    Adding social share

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Facebook from '../SocialShare/Buttons/Facebook.js'
 
 const Home = ( {subDomain} ) => {
   const styles = require(`./brands/${subDomain}/styles.scss`);
@@ -7,6 +7,7 @@ const Home = ( {subDomain} ) => {
   return (
     <div>
       <div className={styles.wrapper}>This is my home section!</div>
+      <Facebook url={"toni-develops.com"} />
     </div>
   )
 }
