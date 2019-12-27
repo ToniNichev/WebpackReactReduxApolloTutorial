2e4252d957033d8a0daaf533b2b9ce4998896037
Toni Nichev <toni.nichev@nbcuni.com>
2019-12-27 15:52:16 -0500

    #stable, added linkedIn

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Facebook from '../SocialShare/Buttons/Facebook.js';
 import Twitter from '../SocialShare/Buttons/Twitter.js';
+import Linkedin from '../SocialShare/Buttons/Linkedin.js';
 
 const Home = ( {subDomain} ) => {
   const styles = require(`./brands/${subDomain}/styles.scss`);
@@ -10,6 +11,7 @@ const Home = ( {subDomain} ) => {
       <div className={styles.wrapper}>This is my home section!</div>
       <Facebook url={"https://www.toni-develops.com/2019/12/17/check-if-string-has-all-unique-characters/"} />
       <Twitter />
+      <Linkedin url={"https://www.toni-develops.com/2019/12/17/check-if-string-has-all-unique-characters/"} />
     </div>
   )
 }
---
+++ src/components/SocialShare/Buttons/Linkedin.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SharingButton from '../Tools/SharingButton';
+import FacebookIcon from '../Icons/Facebook';
+
+const Facebook = (props) => {
+  const text = props.text || 'Linkedin';
+  const url = props.url || window.location.href;
+  const link = `https://www.linkedin.com/sharing/share-offsite/?url=${url}`;
+  return (
+    <SharingButton
+      type="linkedin"
+      link={link}
+      onClick={props.onClick}
+      icon={FacebookIcon}
+      text={text}
+      windowWidth="900"
+      onPopupClose={() => { console.log("Share Popup Closed!") }}
+    />
+  )
+}
+
+Facebook.propTypes = {
+  text: PropTypes.string,
+  url: PropTypes.string,
+  onClick: PropTypes.func,
+}
+
+export default Facebook;
