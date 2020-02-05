9550e67f6ebbad0b82c80c5e20f203dca4bde93f
Toni Nichev <toni.nichev@nbcuni.com>
2020-02-05 16:00:42 -0500

    #stable

--- webpack.api.config.js
+++ webpack.api.config.js
@@ -1,13 +1,6 @@
 const webpack = require('webpack');
 let config = require('./webpack.base.config.js');
 
-config.entry = [
-  '@babel/polyfill',    
-  './src/index.js',
-  'webpack/hot/dev-server',
-  'webpack-dev-server/client?http://localhost:8080/',       
-];
-
 config.plugins = [... [new webpack.HotModuleReplacementPlugin()], ... config.plugins ];
 
 module.exports = config;
