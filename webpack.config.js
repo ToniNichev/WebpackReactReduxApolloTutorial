439238e978a2496791cc34b95d9599df8b2887f0
Toni Nichev <toni.nichev@nbcuni.com>
2020-02-05 13:42:53 -0500

    sourcemap adding

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,7 @@ module.exports = {
             loader: 'postcss-loader',
             options: {
               plugins: () => [require('autoprefixer')()],
+              sourceMap: true
             },
           },
           {
