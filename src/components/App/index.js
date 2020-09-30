390c6939c34ecd0d16702cd2df09a1ad096138a5
ToniNichev <toni.nichev@gmail.com>
2020-09-30 14:34:23 -0400

    Adding context provider and consumer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Greetings from '../Greetings';
+import { ThemeContextProvider } from "../../themeContext";
 import styles from './styles.scss';
 
 export default class App extends Component {
   render() {
     return (
-      <div className={styles.appWrapper}>
-        <h1>React is running</h1>
-        <Greetings user="John" />
-      </div>
+      <ThemeContextProvider>
+        <div className={styles.appWrapper}>
+          <h1>React is running</h1>
+          <Greetings user="John" />
+        </div>
+      </ThemeContextProvider>
     );
   }
 }
--- src/components/Label/index.js
+++ src/components/Label/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import styles from './styles.scss';
+import { ThemeContextConsumer } from "../../themeContext";
 
 function Label(props) {
-  return(<div className={styles.wrapper}>
-          <h2>{props.user}</h2>
-        </div>);
+  return(
+    <ThemeContextConsumer>
+      {context => (
+        <div className={styles.wrapper}>
+            <h2>{context.theme}</h2>
+        </div>
+      )}
+    </ThemeContextConsumer>
+    );     
 }
+
+Label.contextType = ThemeContextConsumer;
+
 export default Label;
